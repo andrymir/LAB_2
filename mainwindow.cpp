7e0d3bcc0f98a22ffe649e6381a99616e0b6f96d
andrymir <100385213+andrymir@users.noreply.github.com>
2022-02-25 01:05:30 +0300

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,31 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "cars.h"
+#include "csvreader.h"
+#include "QString"
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    CSVReader csv("cars.csv");
+    if(csv.is_open())
+    {
+      cars = csv.readAll();
+
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::findCars()
+{
+    for (const auto &c: cars)
+        if(ui->editSearch->text().isEmpty())
+         ui->textBrowser->append(QString::number(c.n)+ ", "+QString::fromStdString(std::string(c.model))+", "+ QString::number(c.year));
+        else if(c.model==ui->editSearch->text().toStdString())
+        ui->textBrowser->append(QString::number(c.n)+ ", "+QString::fromStdString(std::string(c.model))+", "+ QString::number(c.year));
+}
