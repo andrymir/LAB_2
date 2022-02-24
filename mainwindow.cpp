44287a4704fa5b0d2405abbe62818c2020c5adb8
andrymir <100385213+andrymir@users.noreply.github.com>
2022-02-25 01:15:46 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -23,6 +23,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::findCars()
 {
+    ui->textBrowser->clear();
     for (const auto &c: cars)
         if(ui->editSearch->text().isEmpty())
          ui->textBrowser->append(QString::number(c.n)+ ", "+QString::fromStdString(std::string(c.model))+", "+ QString::number(c.year));
